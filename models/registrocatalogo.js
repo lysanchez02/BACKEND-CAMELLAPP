72b0381b0e3aa3ab6ed387fac3f6e672ae645a06
Leyder Sanchez <lysanchez02@misena.edu.co>
2022-10-10 21:07:32 -0500

    primer commit v1.1

---
+++ models/registrocatalogo.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class registrocatalogo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  registrocatalogo.init({
+   
+    titulo: DataTypes.STRING,
+    descripcion: DataTypes.STRING,
+    lugartrabajo: DataTypes.STRING,
+    fechaInicioTrabajo: DataTypes.STRING,
+    tiempoTrabajo: DataTypes.STRING,
+    id_postulante: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'registrocatalogo',
+    tableName: 'registrocatalogo'
+  });
+  return registrocatalogo;
+};
---
+++ models/usuario.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  usuario.init({
+    correo: DataTypes.STRING,
+    contrasena: DataTypes.STRING,
+    rol: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'usuario',
+    tableName: 'usuario'
+  });
+  return usuario;
+};
