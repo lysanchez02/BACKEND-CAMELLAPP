298ae40f5bb8a18a683a334bf8f68820d5e8b2e5
jhonca <capotejhon173@gmail.com>
2022-11-29 19:47:13 -0500

    actualizacion de baken jhon

--- controllers/autenticacioncontroller.js
+++ controllers/autenticacioncontroller.js
@@ -1,17 +1,59 @@
 const{usuario} =require('../models/index');
+const bcrypt = require('bcrypt');
+const jwk = require(jsonwebtoken);
+const authconfig = require('../config/auth');
+const { UPSERT } = require('sequelize/types/query-types');
 
 module.exports = {
     signIn( req, res){
-
+        let { correo, contraseña } = req.body;
+        //buscar usuario
+        Usuario.findOne({
+            where:{
+                correo:correo
+            }
+        }).then(usuario => {
+            if(!usuario){ 
+                res.status(404).json({ msg:"Este usuario no existe"})
+            }else {
+                if(bcrypt.compareSync(contraseña, usuario.contraseña)) {
+                    let token = jwt.sing({usuario: usuario}, authconfig.secreto,{
+                        expiresIn:authconfig.expires  
+                    });
+                    res.json({
+                        usuario:usuario,
+                        token: token
+                    })
+                }else {
+                    res.status(400).json({msg: "contraseña incorrecta"})
+                }
+            }
+        })
     },
-
+    //registar
     signUp(req, res){
-        let password =
+
+        //incriptamos la contraseña
+        let password = bcrypt.hashSync(req.body.password,authconfig.rounds);
+
         usuario.create({
             rol: req.body.rol,
             correo: req.body.correo,
             password:password
-        })
+        }).then(usuario => {
+            // creamos la contraseña
+            
+            let token = jwt.sing({usuario: usuario}, authconfig.secreto,{
+             expiresIn:authconfig.expires   
+            });
+             res.json({
+                usuario:usuario,
+                token: token
+             });
+        }).catch(err => {
+            res.status(500).json(error);
+        });
+
 
     }
 }
